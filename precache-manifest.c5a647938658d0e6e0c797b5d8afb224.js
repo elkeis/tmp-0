240509125e19daf7eaba0fcfcc192f16c54d8856
Kiryl Y <kiryll.e@gmail.com>
2019-08-06 01:37:16 +0200

    Updates

---
+++ precache-manifest.c5a647938658d0e6e0c797b5d8afb224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af5760ba0f1b6593158ff348b66a3b0",
+    "url": "/elkeis/tmp-0/index.html"
+  },
+  {
+    "revision": "cbd4d56ea52b66095d30",
+    "url": "/elkeis/tmp-0/static/css/main.898d9a01.chunk.css"
+  },
+  {
+    "revision": "6ee5f702952d4664c19b",
+    "url": "/elkeis/tmp-0/static/js/2.d7e51c0a.chunk.js"
+  },
+  {
+    "revision": "cbd4d56ea52b66095d30",
+    "url": "/elkeis/tmp-0/static/js/main.113a8fda.chunk.js"
+  },
+  {
+    "revision": "7b0d227d0748534feac3",
+    "url": "/elkeis/tmp-0/static/js/runtime~main.2fda6c8f.js"
+  }
+]);
