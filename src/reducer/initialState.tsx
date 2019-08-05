933c1889e7555aa83c23a428ca459343d94bcacf
Kiryl Y <kiryll.e@gmail.com>
2019-08-05 13:47:26 +0200

    clean up the field

--- src/reducer/initialState.tsx
+++ src/reducer/initialState.tsx
@@ -2,13 +2,13 @@ import {ObstacleType} from '../models';
 import * as t from './types';
 export const INITIAL_STATE: t.State = {
     obstacles: [
-        {row: 0, column: 0, type: ObstacleType.GRAVEL},
-        {row: 2, column: 3, type: ObstacleType.WORMHOLE_ENTRANCE},
-        {row: 5, column: 6, type: ObstacleType.GRAVEL},
-        {row: 5, column: 5, type: ObstacleType.BOULDER},
-        {row: 7, column: 4, type: ObstacleType.WORMHOLE_EXIT},
-        {row: 9, column: 1, type: ObstacleType.WORMHOLE_ENTRANCE},
-        {row: 0, column: 9, type: ObstacleType.WORMHOLE_EXIT},
+        // {row: 0, column: 0, type: ObstacleType.GRAVEL},
+        // {row: 2, column: 3, type: ObstacleType.WORMHOLE_ENTRANCE},
+        // {row: 5, column: 6, type: ObstacleType.GRAVEL},
+        // {row: 5, column: 5, type: ObstacleType.BOULDER},
+        // {row: 7, column: 4, type: ObstacleType.WORMHOLE_EXIT},
+        // {row: 9, column: 1, type: ObstacleType.WORMHOLE_ENTRANCE},
+        // {row: 0, column: 9, type: ObstacleType.WORMHOLE_EXIT},
     ],
     grid: {
         columnsCount: 10,
@@ -24,18 +24,18 @@ export const INITIAL_STATE: t.State = {
             column: 9
         },
         path: [
-            {row: 0, column: 0},
-            {row: 0, column: 1},
-            {row: 1, column: 1},
-            {row: 1, column: 2},
-            {row: 2, column: 2},
-            {row: 3, column: 2},
-            {row: 3, column: 3},
-            {row: 3, column: 4},
-            {row: 4, column: 4},
+            // {row: 0, column: 0},
+            // {row: 0, column: 1},
+            // {row: 1, column: 1},
+            // {row: 1, column: 2},
+            // {row: 2, column: 2},
+            // {row: 3, column: 2},
+            // {row: 3, column: 3},
+            // {row: 3, column: 4},
+            // {row: 4, column: 4},
         ]
     },
-    gridControlAction: t.GridActionTypeKeys.ADD_BOULDER
+    gridControlAction: undefined//t.GridActionTypeKeys.ADD_BOULDER
 }
 
 export enum ScreenType  {
